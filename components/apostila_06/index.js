7c3ba8959ef105f21bd29c3c3b34b10c8a386928
Lucas-Costa-Xavier <124743196+Lucas-Costa-Xavier@users.noreply.github.com>
2023-06-13 21:05:31 -0300

    calc imc

--- components/apostila_06/index.js
+++ components/apostila_06/index.js
@@ -15,7 +15,18 @@ export default function Index() {
 
         if (valor < 18.5){
             setPorcentual('Abaixo do peso');
-        }
+        }else if (valor >= 18 && valor < 24.59){
+                setPorcentual('Peso Normal');
+            } else if (valor >= 25 && valor < 29.9 ){
+                setPorcentual('sobrepeso');
+            } else if (valor >= 30 && valor < 34.9){
+                setPorcentual('Obesidade grau 1');
+            } else if (valor >= 35 && valor < 39.9){
+                setPorcentual('Obesidade grau 2');
+            } else if (valor > 40){
+                setPorcentual('Obesidade grau 3');
+            }
+        
     }
         
         
